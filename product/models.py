3af2e1f9dee9f01d4680a2c901017c7d826eb322
Orxan <anvarliorxan@gmail.com>
2024-05-07 15:49:41 +0400

    last version

---
+++ product/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    price = models.IntegerField()
+    discount = models.IntegerField(blank=True, null=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
