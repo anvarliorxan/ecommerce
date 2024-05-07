31f64a83553f657e15a37e1d510fd50a101794f4
Orxan <anvarliorxan@gmail.com>
2024-05-07 15:56:29 +0400

    added blank, null true

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     price = models.IntegerField()
     discount = models.IntegerField(blank=True, null=True)
